159f4885538cd206b1057aa797360fa4a3a222d2
Revanee <hkamenov123@gmail.com>
2017-05-24 13:32:54 +0200

    Menu now shows on hover

--- scr/menu.js
+++ scr/menu.js
@@ -30,32 +30,60 @@ class Menu {
 
         if (this.hover()) {
             plane(this.getSize() * 1.2, this.getSize() * 1.2)
+
+            this.drawBest(this.getSize() * 0.6, this.getSize() * 0.6)
+
+            this.drawLast(this.getSize() * 0.6, this.getSize() * 0.6)
         } else {
-            //Draw play button
             plane(this.getSize(), this.getSize())
 
-            //Draw best score
-            push()
-            texture(sprites.menu.best)
-            if (isLandscape()) {
-                translate(-this.getSize(), 0)
-            } else {
-                translate(0, -this.getSize())
-            }
-            plane(this.getSize(), this.getSize())
-            this.drawNumber(this.parent.score.best)
-            pop()
+            this.drawBest(this.getSize() * 0.8, this.getSize() * 0.8)
 
-            //Draw last score
+            this.drawLast(this.getSize() * 0.8, this.getSize() * 0.8)
+
+        }
+    }
+    drawBest(sizeX, sizeY) {
+        //Draw best score
+        push()
+        texture(sprites.menu.best)
+        if (isLandscape()) {
+            translate(-this.getSize(), 0)
+        } else {
+            translate(0, -this.getSize())
+        }
+        plane(sizeX, sizeY)
+        this.drawNumber(this.parent.score.best, sizeX / 2, sizeY)
+        pop()
+
+    }
+    drawLast(sizeX, sizeY) {
+        //Draw last score
+        push()
+        texture(sprites.menu.last)
+        if (isLandscape()) {
+            translate(this.getSize(), 0)
+        } else {
+            translate(0, this.getSize())
+        }
+        plane(sizeX, sizeY)
+        this.drawNumber(this.parent.score.last, sizeX / 2, sizeY)
+        pop()
+
+    }
+    drawNumber(num, sizeX, sizeY) {
+        if (!(num > 100 || num < 0)) {
+            let leftNum = Math.floor(num / 10)
+            let rightNum = num - leftNum * 10
+
             push()
-            if (isLandscape()) {
-                translate(this.getSize(), 0)
-            } else {
-                translate(0, this.getSize())
-            }
-            texture(sprites.menu.last)
-            plane(this.getSize(), this.getSize())
-            this.drawNumber(this.parent.score.last)
+            translate(sizeX / 2, 0)
+            texture(sprites.numbers[rightNum])
+            plane(sizeX, sizeY)
+
+            translate(-sizeX, 0)
+            texture(sprites.numbers[leftNum])
+            plane(sizeX, sizeY)
             pop()
         }
     }
