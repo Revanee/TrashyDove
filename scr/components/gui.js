aca765a70d558423fa95e01bc697f683c0a763d0
Revanee <hkamenov123@gmail.com>
2017-10-11 13:16:22 +0200

    Better gui

--- scr/components/gui.js
+++ scr/components/gui.js
@@ -1,21 +1,4 @@
 function drawNumber(num, size) {
-    // if (!(num >= 100 || num < 0)) {
-    //     let leftNum = Math.floor(num / 10)
-    //     let rightNum = num - leftNum * 10
-
-    //     push()
-    //     translate(sizeX / 2, 0)
-    //     texture(sprites.numbers[rightNum])
-    //     plane(sizeX, sizeY)
-
-    //     translate(-sizeX, 0)
-    //     texture(sprites.numbers[leftNum])
-    //     plane(sizeX, sizeY)
-    //     pop()
-    // }
-
-
-    //new version
     if (num < 0) {
         console.log('Drawing invalid number!')
         return -1
